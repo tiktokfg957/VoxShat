1b6219d66f0fcc17500f374e19ecbc3f424675f5
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-08 02:40:40 +0700

    Update User.kt

--- app/src/main/java/com/example/voxshat/data/model/User.kt
+++ app/src/main/java/com/example/voxshat/data/model/User.kt
@@ -5,13 +5,10 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
 data class User(
-    @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
-    var name: String,
-    var username: String = "",
-    var avatar: String? = null,
-    var status: String = "онлайн",
-    var role: String = "user", // user, support, moderator, admin, business, partner, celebrity
-    var verifiedType: String? = null, // support, moderator, admin, business, partner, celebrity
-    var verifiedAt: Long? = null
+    @PrimaryKey
+    val uid: String,
+    val phoneNumber: String,
+    val name: String = "",
+    val photoUrl: String? = null,
+    val status: String = "онлайн"
 )
