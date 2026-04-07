5bde97bb86f23bdb42a5350916546972973f9fc4
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-08 02:41:44 +0700

    Update UserDao.kt

--- app/src/main/java/com/example/voxshat/data/database/UserDao.kt
+++ app/src/main/java/com/example/voxshat/data/database/UserDao.kt
@@ -9,15 +9,15 @@ interface UserDao {
     @Query("SELECT * FROM users")
     fun getAllUsers(): Flow<List<User>>
 
-    @Insert
+    @Query("SELECT * FROM users WHERE uid = :uid")
+    suspend fun getUser(uid: String): User?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(user: User)
 
     @Update
     suspend fun update(user: User)
 
-    @Query("SELECT * FROM users WHERE id = :userId")
-    suspend fun getUserById(userId: Long): User?
-
-    @Query("SELECT * FROM users WHERE username = :username")
-    suspend fun getUserByUsername(username: String): User?
+    @Delete
+    suspend fun delete(user: User)
 }
