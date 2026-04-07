7529488889613eb18d241029087dc9314798eb40
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-08 02:43:10 +0700

    Update VoxShatApplication.kt

--- app/src/main/java/com/example/voxshat/VoxShatApplication.kt
+++ app/src/main/java/com/example/voxshat/VoxShatApplication.kt
@@ -2,9 +2,9 @@ package com.example.voxshat
 
 import android.app.Application
 import com.example.voxshat.data.database.AppDatabase
+import com.example.voxshat.data.repository.ChatRepository
 
 class VoxShatApplication : Application() {
-    val database: AppDatabase by lazy {
-        AppDatabase.getDatabase(this)
-    }
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val chatRepository by lazy { ChatRepository(database) }
 }
