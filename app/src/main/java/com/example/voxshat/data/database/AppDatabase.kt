932f2361dec4cc99f30248e3be86bf968ab67f50
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-10 03:36:22 +0700

    Update AppDatabase.kt

--- app/src/main/java/com/example/voxshat/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/voxshat/data/database/AppDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.example.voxshat.data.model.Chat
 import com.example.voxshat.data.model.Message
 import com.example.voxshat.data.model.User
@@ -14,7 +13,6 @@ import com.example.voxshat.data.model.User
     version = 1,
     exportSchema = false
 )
-@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun chatDao(): ChatDao
@@ -29,7 +27,7 @@ abstract class AppDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "voxshat_db"
+                    "voxshat_database"
                 ).fallbackToDestructiveMigration()
                     .build()
                 INSTANCE = instance
