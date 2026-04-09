24e2ca1129d3dfd911eb42eab98150c8f4af7c0d
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-10 03:40:35 +0700

    Update AppDatabase.kt

--- app/src/main/java/com/example/voxshat/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/voxshat/data/database/AppDatabase.kt
@@ -4,15 +4,17 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.voxshat.data.model.Chat
 import com.example.voxshat.data.model.Message
 import com.example.voxshat.data.model.User
 
 @Database(
     entities = [User::class, Chat::class, Message::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun chatDao(): ChatDao
